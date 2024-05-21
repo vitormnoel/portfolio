9df8584ef7bb61c5054cd6491440762a0697bca8
vitormnoel <vtormnoel@gmail.com>
2024-05-21 17:25:40 +0200

    navbar position

--- app/components/header/navbar.jsx
+++ app/components/header/navbar.jsx
@@ -43,11 +43,11 @@ function navBar() {
         >
           <CiMenuFries size={22} className="cursor-pointer" />
           {navbar ? (
-            <div className="overscroll-y-none overflow-y-hidden fixed right-0 lef-0 top-18 bg-white w-full">
-              <div className="flex flex-col items-center pt-16 gap-2 h-full">
+            <div className="bg-white  absolute left-0 right-0">
+              <div className="flex flex-col items-center justify-center pt-16 gap-2 h-full">
                 <Link
                   href="/about"
-                  className="hover:bg-green-pastel w-full p-4"
+                  className="hover:bg-sky-100 w-full p-4"
                 >
                   about
                 </Link>
