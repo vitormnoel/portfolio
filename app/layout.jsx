e4dcf6c7c7a92962455599625d9a82dfe3b7d92a
vitor <hi@vitormanoel.com>
2025-03-04 15:46:48 +0100

    adding statcounter script

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import "./style/globals.css";
 
+import Script from "next/script";
+
 import Footer from "./components/footer/footer.jsx";
 import NavBar from "./components/header/navbar.jsx";
 
@@ -36,6 +38,15 @@ export default function rootLayout({ children }) {
         <NavBar />
         <main className="container mx-auto px-8 lg:w-2/3] ">{children}</main>
         <Footer />
+
+        <Script>
+          {`var sc_project=13098240; var sc_invisible=1; var
+              sc_security="3327c779"`}
+        </Script>
+        <Script
+          src="https://www.statcounter.com/counter/counter.js"
+          async
+        ></Script>
       </body>
     </html>
   );
